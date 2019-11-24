cc8693041ef8cb8a2c09aa0f97da73ca2aadfc5f
Swapnil Raj <raj.swapnil99@gmail.com>
2019-11-24 23:30:53 +0000

    Add documentation to AIMine

--- app/AIMine.hs
+++ app/AIMine.hs
@@ -1,4 +1,4 @@
-{-
+{-|
   An indirection to not expose any of the information that the AI can use to
   solve the board.
   The exports from this module, make the board look like as a human is looking
@@ -9,13 +9,17 @@
 -}
 module AIMine
   ( M.Board
+  -- | Re-export type from module "Mine"
   , Cell(..)
   , M.Point(..)
+  -- | Re-export type from module "Mine"
   , (!?)
   , allHidden
   , assocs
   , M.clip
+  -- | Re-export utility function from module "Mine"
   , M.genNeighbours
+  -- | Re-export utility function from module "Mine"
   , size
   ) where
 
@@ -33,25 +37,41 @@ import qualified Mine as M
 import Prelude hiding(foldl)
 import qualified Data.Map.Strict as Map(assocs, foldl)
 
-data Cell -- Too confusing if its the same name?
-  = Hidden
-  | Empty
-  | Flagged
-  | Numbered Int
+-- | Too confusing if its the same name? This represents a cell from a human's
+-- perspective. The AI should have the same information as a human who looks at
+-- a rendered board.
+data Cell
+  = Hidden -- ^ An unrevealed cell
+  | Empty -- ^ A revealed cell, which is empty
+  | Flagged -- ^ An unrevealed cell, with a flag on it
+  | Numbered Int -- ^ A cell adjacent to a mine, where the number represents
+  --how many mines there are close to it
   deriving (Eq, Show)
 
+-- | The size of the board, the length of the board to be more precise; a board
+-- is guaranteed to be a square. Utility function to avoid exposing the
+-- constructor for 'M.Board' to "AI".
 size :: M.Board -> Int
 size b = snd $ M.unBoard b
 
+-- | Checks if the all the cells in a board are 'Hidden'
+-- Utility function to exposing the constructor for 'M.Board' to "AI".
 allHidden :: M.Board -> Bool
 allHidden b' = let (b, _) = M.unBoard b' in
                    Map.foldl (\acc val -> acc && (humanVision val == Hidden))
                           True b
 
+-- | Similar to 'Map.assocs' but for 'M.Board' type.
+-- Utility function to exposing the constructor for 'M.Board' to "AI".
 assocs :: M.Board -> [(M.Point, Cell)]
 assocs b' = let (b, _) = M.unBoard b'
              in (\(p, c) -> (p, humanVision c)) <$> Map.assocs b
 
+-- | Applies human vision to the underlying 'Map' in 'M.Board'.
+-- For examples turns a value of 'M.Empty Hidden' turns to 'Hidden'.
+-- Errors out if a 'M.Mine' is seen as the AI should never be invoked in a case
+-- where a mine was exposed.
+-- This function is the main purpose of the of this module.
 humanVision :: M.Cell -> Cell
 humanVision c
   | M.isHiddenCell c = Hidden
@@ -61,5 +81,8 @@ humanVision c
                   M.Numbered n _ -> Numbered n
                   M.Mine{} -> error "Ooft! you've already lost"
 
+-- | Override the infix lookup function exported by Data.Map.
+-- As the type signature suggests, it applies 'humanVision' to value obtained
+-- by looking up in the Map.
 (!?) :: M.Board -> M.Point -> Cell
 (!?) b p = humanVision . flip (M.!?) p $ fst $ M.unBoard b
